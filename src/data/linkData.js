644e0aeb11c0fabc2d5f1e7bb1b05b7d85739bbf
Robert Toth-Ludanyi <roberttothmails@yahoo.com>
2024-04-02 16:31:19 +0200

    Done

--- src/data/linkData.js
+++ src/data/linkData.js
@@ -2,7 +2,9 @@
   { href: '#', className: 'nav-item', text: 'Home' },
   { href: '#', className: 'nav-item', text: 'About' },
   { href: '#', className: 'nav-item', text: 'Blog' },
-  { href: '#', className: 'nav-item', text: 'Contact' },
+  { href: '#', className: 'nav-item', text: 'Our Story' },
+  { href: '#', className: 'nav-item', text: 'Donate' },
+  { href: '#', className: 'nav-item', text: 'Contact' }
 ]
 
 export default linkData
